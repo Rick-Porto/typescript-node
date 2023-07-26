1fc785fc6b6bf96eb5e6501554895c1d613e1e50
Rick Porto <rickmarco4@hotmail.com>
2023-07-26 10:56:23 -0300

    Fixed transferência in Application 5

--- src/application/main05.ts
+++ src/application/main05.ts
@@ -3,14 +3,14 @@ import ContaCorrente from '../entities/contaCorrente';
 
 // Criando o primeiro cliente com conta corrente
 const cliente5ContaCorrente1 = new Cliente('111.222.333-44', 'Cliente 5 (Conta Corrente 1)', '(11) 98765-4321', true);
-const contaCorrente4 = new ContaCorrente('1234', cliente5ContaCorrente1, 100); // O último argumento é o limite
+const contaCorrente4 = new ContaCorrente('1234', cliente5ContaCorrente1, 100); 
 
 // Efetuando um depósito de 300 na primeira conta corrente
 contaCorrente4.depositar(300);
 
 // Criando o segundo cliente com conta corrente
 const cliente5ContaCorrente2 = new Cliente('444.555.666-77', 'Cliente 5 (Conta Corrente 2)', '(11) 98765-4321', false);
-const contaCorrente5 = new ContaCorrente('5678', cliente5ContaCorrente2, 100); // O último argumento é o limite
+const contaCorrente5 = new ContaCorrente('5678', cliente5ContaCorrente2, 100); 
 
 // Efetuando um depósito de 100 na segunda conta corrente
 contaCorrente5.depositar(100);
@@ -18,9 +18,7 @@ contaCorrente5.depositar(100);
 // Tentando efetuar a transferência de 1000 reais da primeira conta corrente para a segunda conta corrente
 const valorTransferencia = 1000;
 if (contaCorrente4.calcularSaldo() >= valorTransferencia) {
-  contaCorrente4.sacar(valorTransferencia);
-  contaCorrente5.depositar(valorTransferencia);
-  console.log(`Transferência de R$ ${valorTransferencia} realizada com sucesso.`);
+  contaCorrente4.transferencia(contaCorrente5, valorTransferencia);
 } else {
   console.log('Saldo insuficiente na primeira conta corrente para efetuar a transferência.');
 }
@@ -28,4 +26,6 @@ if (contaCorrente4.calcularSaldo() >= valorTransferencia) {
 // Exibindo o saldo atualizado das duas contas correntes
 console.log('Saldo da Conta Corrente 1:', contaCorrente4.calcularSaldo());
 console.log('Saldo da Conta Corrente 2:', contaCorrente5.calcularSaldo());
+
+
 
